9d9f7b8e3abb4e22a5cb8976245c0dd718256203
MikalDev <danpshimizu@gmail.com>
2024-07-26 09:20:25 -0700

    fix (mass) change set mass to apply to collider, not body

--- export/editor.js
+++ export/editor.js
@@ -3,7 +3,7 @@ const SDK = self.SDK;
 const BEHAVIOR_INFO = {
     ...{
   "id": "mikal_cannon_3d_physics",
-  "version": "2.14.1",
+  "version": "2.15.1",
   "category": "movements",
   "author": "Mikal",
   "addonType": "behavior",
